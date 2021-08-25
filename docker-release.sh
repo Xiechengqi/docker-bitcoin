a574cd7e26b66f05321d14e7095b80091457c309
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-25 12:52:14 +0800

    Update docker-release.sh

--- docker-release.sh
+++ docker-release.sh
@@ -12,8 +12,8 @@ source <(curl -SsL $BASEURL/tool/common.sh)
 latest_version=$(curl -SsL https://bitcoin.org/en/download | grep 'Latest version:' | awk '{print $NF}')
 # curl -SsL https://registry.hub.docker.com/v1/repositories/fullnode/bitcoin/tags | grep ${latest_version} &> /dev/null && exit 0
 
-INFO "docker build --force-rm --no-cache --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg VERSION=${latest_version} -t fullnode/bitcoin:${latest_version} -f ./Dockerfile ."
-docker build --force-rm --no-cache --build-arg BUILD_DATE="China-$(TZ=UTC-8 date +'%Y-%m-%dT%H:%M:%SZ')" --build-arg VERSION=${latest_version} -t fullnode/bitcoin:${latest_version} -f ./Dockerfile . || exit 1
+INFO "docker build --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg VERSION=${latest_version} -t fullnode/bitcoin:${latest_version} -f ./Dockerfile ."
+docker build --build-arg BUILD_DATE=$(TZ=UTC-8 date +'%Y-%m-%d-%H:%M:%S') --build-arg VERSION=${latest_version} -t fullnode/bitcoin:${latest_version} -f ./Dockerfile . || exit 1
 EXEC "docker tag fullnode/bitcoin:${latest_version} fullnode/bitcoin:latest"
 EXEC "docker push fullnode/bitcoin:${latest_version}"
 EXEC "docker push fullnode/bitcoin:latest"
