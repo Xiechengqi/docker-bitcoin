d2fb78ef6d82b4da76d9e20a6153493999c50cf2
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-25 13:35:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@
 # 2021/08/24
 #
 
+# check data dir if empty, if not empty return error
+[ "$(ls -A /bitcoin/data)" != "" ] && echo "ERROR: /bitcoin/data is not empty, Please backup it first and remove all old data" && exit 1
+
 if [ "$1" = "testnet" ]; then
 bitcoind -conf=/bitcoin/conf/bitcoin.conf --testnet
 else
